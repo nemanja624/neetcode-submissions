1e38ab4373dd2503a95e81658862b8ccfa0b79f5
Nemanja Mirkovic <nemanja.nm123@gmail.com>
2026-05-17 19:06:09 +0200

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,39 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = [[p, s] for p, s in zip(position, speed)]
+        cars.sort(reverse=True)
+        stack = []
+
+        for p, s in cars:
+            time = (target - p) / s
+            if not stack or time > stack[-1]:
+                stack.append(time)
+    
+        return len(stack)
+
+
+
+
+
+
+
+
+
+
+
+
+    # parovi position, speed
+
+    # vreme
+
+    # ako ga stize, nemoj ga apendovati
+
+    # vrati duzinu steka
+
+        #HINT
+        # sort od najblizeg ka najdaljem
+        # parovi position, speed
+        # time = (target - position) / speed
+        # ako je time veci ili je stek prazan, apenduj
+        # vrati length
+
