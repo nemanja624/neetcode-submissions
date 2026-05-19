d6ccd688703d60cc63f0988253ba960c8716ab64
Nemanja Mirkovic <nemanja.nm123@gmail.com>
2026-05-19 16:52:40 +0200

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.py
@@ -0,0 +1,47 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        left = 0
+        longest = 0
+        my_dict = {}
+        max_frequency = 0
+
+        for right in range(len(s)):
+            if s[right] in my_dict:
+                my_dict[s[right]] += 1
+            else:
+                my_dict[s[right]] = 1
+
+            max_frequency = max(max_frequency, my_dict[s[right]])
+
+            while (right - left + 1) - max_frequency > k:
+                my_dict[s[left]] -= 1
+                left += 1
+
+            longest = max(longest, right - left + 1)
+
+        return longest
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # recnik pamti right koji se pomera
+        # nadji max_frequency 
+        # while prozor - max_freq > k:
+        # pomeri left unapred, i izbaci element koji nije vise u prozoru iz recnika
+        # longest = max(longest, right - left + 1)
+        
