98f81c9663b54ed77bd4f7ea7621479098ef02b3
Nemanja Mirkovic <nemanja.nm123@gmail.com>
2026-05-21 18:11:10 +0200

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left = 0
+        right = 1
+        max_profit = 0
+
+        for right in range(len(prices)):
+            if prices[left] > prices[right]:
+                left = right
+            else:
+                profit = prices[right] - prices[left]
+                if profit > max_profit:
+                    max_profit = profit
+
+        return max_profit
+
