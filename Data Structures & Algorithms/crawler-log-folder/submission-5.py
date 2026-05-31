b91d558c18ad9f6a9614c393bc29a41d233b9110
Nemanja Mirkovic <nemanja.nm123@gmail.com>
2026-05-31 17:30:06 +0200

    Add: crawler-log-folder - submission-5

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack = []
+
+        for log in logs:
+            if log == "./":
+                continue
+            elif log == "../":
+                if not stack:
+                    continue
+                stack.pop()
+
+            else:
+                stack.append(log)
+                print(stack)
+
+        return len(stack)
