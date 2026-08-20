eb431cf0df256a4997e46b0e60537c48d62829b4
Nemanja Mirkovic <nemanja.nm123@gmail.com>
2026-08-20 16:36:33 +0200

    Bulk sync: 474 submissions

--- Data Structures & Algorithms/balanced-binary-tree/submission-1.py
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.py
@@ -6,28 +6,23 @@
 #         self.right = right
 
 class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
-            return True
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
 
-        if not p or not q:
-            return False
+        def dfs(root):
+            if not root:
+                return 0
+            
+            left = dfs(root.left)
+            if left == -1:
+                return -1
+            
+            right = dfs(root.right)
+            if right == -1:
+                return -1
 
-        queue = ([(p, q)])
+            if abs(left - right) > 1:
+                return -1
 
-        while queue:
-            n1, n2 = queue.pop()
+            return max(left, right) + 1
 
-            if not n1 and not n2:
-                continue
-
-            if not n1 or not n2:
-                return False
-
-            if n1.val != n2.val:
-                return False
-
-            queue.append((n1.left, n2.left))
-            queue.append((n1.right, n2.right))
-
-        return True
+        return dfs(root) != -1
